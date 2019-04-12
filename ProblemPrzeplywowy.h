2c9d5440b1a28973656d384de4b0e553d32bf22a
SPDPiech <48133682+SPDPiech@users.noreply.github.com>
2019-04-12 15:33:07 +0200

    Add files via upload

--- ProblemPrzeplywowy.h
+++ ProblemPrzeplywowy.h
@@ -8,7 +8,9 @@
 #include <vector>
 #include <algorithm>
 #include <windows.h>
-
+#include <math.h>
+#include <ctime>
+#include <iomanip>
 using namespace std;
 
 class ProblemPrzeplywowy
@@ -26,10 +28,24 @@ public:
     void WczytajDane(string NazwaPliku);
     uint32_t PrzegladZupelny(string* Kolejnosc);
     uint32_t AlgorytmJohnsona(string* NajlepszaKolejnosc);
+    uint32_t AlgorytmNEH(string* NajlepszaKolejnosc);
+    uint32_t AkceleracjaNEH(string* NajlepszaKolejnosc);
+    uint32_t ZmodyfikowanyNEH(string* NajlepszaKolejnosc);
+    uint32_t AlgorytmWyzarzaniaNEH(string* NajlepszaKolejnosc, float PoczatkowaTemperatura, float ParametrChlodzenia, float TemperaturaGraniczna, bool UzyjSWAP);
+    uint32_t AlgorytmWyzarzaniaLosowy(string* NajlepszaKolejnosc, float PoczatkowaTemperatura, float ParametrChlodzenia, float TemperaturaGraniczna, bool UzyjSWAP);
     void GenerujLosoweDane(uint32_t LiczbaZadan, uint32_t LiczbaMaszyn);
     void TestAlgorytmow(uint32_t MaxLiczbaZadan, uint32_t MaxLiczbaMaszyn);
+    void Compare(string file);
+
 
 };
 
+struct DaneNEH
+{
+    uint32_t NumerZadania;
+    uint32_t Wartosc;
+
+    DaneNEH(uint32_t NowyNumer, uint32_t NowaWartosc){NumerZadania = NowyNumer, Wartosc = NowaWartosc;}
+};
 
 #endif // PROBLEMPRZEPLYWOWY_H
